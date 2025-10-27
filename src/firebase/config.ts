06dbd4cd3064a8f04a34f4f9a8fc6b27061a1c2c
Amit Mhaske <amit.mhaske@encureit.com>
2025-10-27 20:09:56 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9806115625-5189d",
+  "appId": "1:1021397933951:web:9af246fc7b53816df9044a",
+  "apiKey": "AIzaSyDLKi9OP5YMSEmwGbV0yWVn8OHJ-iUHINQ",
+  "authDomain": "studio-9806115625-5189d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1021397933951"
+};
